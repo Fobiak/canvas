a0a14aeb373b0a71a9bd245e223fa947a4b64c7d
Сайсанов Дмитрий Алексеевич <saysanov2018@mail.ru>
2026-02-05 14:10:33 +0500

    bugfix: небольшие исправления

--- src/pages/CanvasPage.vue
+++ src/pages/CanvasPage.vue
@@ -3,7 +3,7 @@ import { Canvas, CanvasControls } from '../features/Canvas';
 </script>
 
 <template>
-    <div class="flex h-screen overflow-hidden justify-center gap-2 py-6 px-4">
+    <div class="flex h-screen overflow-hidden justify-center gap-4 py-6 px-4">
         <Canvas class="flex-1" />
         <div class="shrink-0">
             <CanvasControls />
